d14240edcc58ef8e0240cb4dfd8ec781a6e7a07e
KSymph <flicsmo@vivaldi.net>
2024-07-24 10:43:19 -0400

    basic testing

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Pictureboard</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="script.js" defer></script>
+</head>
+<body>
+	<main>
+		<h1>Hello world!</h1>
+
+
+	</main>
+
+</body>
+</html>
