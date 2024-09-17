3cabe548567d743202a8ee5de588310dad045d38
KSymph <flicsmo@vivaldi.net>
2024-09-17 10:48:25 -0400

    add precaching to service worker

--- sw.js
+++ sw.js
@@ -2,6 +2,24 @@
 
 const CACHE = "pwabuilder-offline";
 
+const CACHE_NAME = 'cool-cache';
+
+// Add whichever assets you want to pre-cache here:
+const PRECACHE_ASSETS = [
+    '/assets/',
+    "/tiles/",
+    "/boards/"
+]
+
+// Listener for the install event - pre-caches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
+
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 self.addEventListener("message", (event) => {
