ae13abf56859d01f6fc19c99efaf02c6f8601bf4
KSymph <flicsmo@vivaldi.net>
2024-10-01 10:44:40 -0400

    update

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ file_put_contents("boards.json", json_encode(generateBoardsJson()));
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="An app designed for speech therapy, in which drawn images are tapped to reveal a random live-action video of the item represented.">
+	<meta name="description" content="PictureBoard is a speech therapy app featuring grids of drawn images. Tap an image to view a random real-life video of the object. New boards added regularly.">
 	<link rel="author" href="https://kwikle.me">
 	<meta name="robots" content="index,nofollow">
 	<title>PictureBoard</title>
