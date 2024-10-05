0c7fef35f5ca2bc1d7bd0c71751394ffb37fed91
KSymph <flicsmo@vivaldi.net>
2024-10-05 03:12:32 -0400

    test

--- test.php
+++ test.php
@@ -28,7 +28,7 @@
 	debutton.addEventListener("click", () => {
 		output.innerHTML = `
 			<video loop muted autoplay playsInLine>
-				<source src="/assets/test.mp4" type="video/mp4">
+				<source src="/assets/apple.mp4" type="video/mp4">
 			</video>
 		`;
 	});
